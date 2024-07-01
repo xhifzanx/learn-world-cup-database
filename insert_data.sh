1e82785c8a23a8c9381cad444bdb69bfea96b277
Mohammad hifzan <misterj435@gmail.com>
2024-07-01 14:04:22 +0000

    insert and quries changes

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,42 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+echo $($PSQL "TRUNCATE teams, games;")
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != "year" ]]
+  then
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER';")
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT';")
+    if [[ -z $WINNER_ID ]]
+    then
+      INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      if [[ $INSERT_WINNER_RESULT == "INSERT 0 1" ]]
+      then
+        WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER';")
+        echo Inserted into teams, $WINNER
+      fi
+    fi
+    if [[ -z $OPPONENT_ID ]]
+    then
+      INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      if [[ $INSERT_OPPONENT_RESULT == "INSERT 0 1" ]]
+      then
+        OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT';")
+        echo Inserted into teams, $OPPONENT
+      fi
+    fi
+  
+    GAME_ID=$($PSQL "SELECT game_id FROM games WHERE (year=$YEAR AND round='$ROUND' AND winner_id=$WINNER_ID AND opponent_id=$OPPONENT_ID AND winner_goals=$WINNER_GOALS AND opponent_goals=$OPPONENT_GOALS);")
+    if [[ -z $GAME_ID ]]
+    then
+      INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS);")
+      if [[ $INSERT_OPPONENT_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into games, $YEAR
+      fi
+    else
+      echo -e "\n\n\nTHIS ONE\N"
+    fi
+  fi
+done
--- queries.sh
+++ queries.sh
@@ -8,34 +8,34 @@ echo -e "\nTotal number of goals in all games from winning teams:"
 echo "$($PSQL "SELECT SUM(winner_goals) FROM games")"
 
 echo -e "\nTotal number of goals in all games from both teams combined:"
-echo
+echo "$($PSQL "SELECT (SUM(winner_goals)+SUM(opponent_goals)) FROM games;")"
 
 echo -e "\nAverage number of goals in all games from the winning teams:"
-echo
+echo "$($PSQL "SELECT ROUND(AVG(winner_goals), 16) FROM games")"
 
 echo -e "\nAverage number of goals in all games from the winning teams rounded to two decimal places:"
-echo
+echo "$($PSQL "SELECT ROUND(AVG(winner_goals), 2) FROM games")"
 
 echo -e "\nAverage number of goals in all games from both teams:"
-echo
+echo "$($PSQL "SELECT ROUND(AVG(winner_goals)+AVG(opponent_goals), 16) FROM games;")"
 
 echo -e "\nMost goals scored in a single game by one team:"
-echo
+echo "$($PSQL "SELECT MAX(winner_goals) FROM games;")"
 
 echo -e "\nNumber of games where the winning team scored more than two goals:"
-echo
+echo "$($PSQL "SELECT COUNT(*) FROM games WHERE winner_goals > 2;")"
 
 echo -e "\nWinner of the 2018 tournament team name:"
-echo
+echo "$($PSQL "SELECT winner_team.name FROM games LEFT JOIN teams AS winner_team ON games.winner_id = winner_team.team_id LEFT JOIN teams AS loser_team ON games.opponent_id = loser_team.team_id WHERE round = 'Final' AND year = 2018;")"
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo
+echo "$($PSQL "SELECT DISTINCT t.name FROM games AS g INNER JOIN teams AS t ON (g.winner_id = t.team_id OR g.opponent_id = t.team_id) WHERE round = 'Eighth-Final' AND year = 2014 ORDER BY t.name;")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
-echo
+echo "$($PSQL "SELECT DISTINCT t.name FROM games AS g INNER JOIN teams AS t ON (g.winner_id = t.team_id) ORDER BY t.name;")"
 
 echo -e "\nYear and team name of all the champions:"
-echo
+echo "$($PSQL "SELECT g.year, t.name FROM games AS g INNER JOIN teams AS t ON (g.winner_id = t.team_id AND round = 'Final') ORDER BY g.year;")"
 
 echo -e "\nList of teams that start with 'Co':"
-echo
+echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%';")"
